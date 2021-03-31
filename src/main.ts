5a9a7dd0ac313597acb40288da07934256adb66e
mgovea <1916712+mgovea@users.noreply.github.com>
2021-03-31 16:09:56 -0600

    Don't call UI for servers where it doesn't exist

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { updateRidePrices } from './ridePriceFunctions';
 import showWindow from './window';
 
 function main(): void {
-  ui.registerMenuItem('Ride Price Manager', () => {
-    showWindow();
-  });
+  if (ui) {
+    ui.registerMenuItem('Ride Price Manager', () => {
+      showWindow();
+    });
+  }
 
   context.subscribe('interval.day', () => {
     updateRidePrices();
