d1a19714050ff3da136e0a03aeb3de9d0798b868
mgovea <1916712+mgovea@users.noreply.github.com>
2021-04-01 17:54:16 -0600

    increment version number

--- src/registerPlugin.ts
+++ src/registerPlugin.ts
@@ -4,7 +4,7 @@ import main from './main';
 
 registerPlugin({
   name: 'Ride Price Manager',
-  version: '1.3.0',
+  version: '1.3.1',
   authors: ['mgovea', 'Sadret'],
   type: 'remote',
   licence: 'MIT',
